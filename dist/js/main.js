6800e87f48a2f60fed8fe0fb1fa9bbc45b5471cf
Nicolaica Catalin Mircea <ncatalin_1988@yahoo.com>
2020-05-11 18:51:40 +0300

    reworked challange as a mobile first approach

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,18 +1,20 @@
-// Get ID's
-const form = document.getElementById('main-form');
-const email = document.getElementById('email');
-// Regex from stackoverflow
-function validateEmail(email) {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+const mainForm = document.getElementById("main-form");
+const email = document.getElementById("email");
+
+function emailValidation(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 }
-// Check if valid email, add/remove class
-form.addEventListener('submit', e => {
+
+mainForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const emailValidation = email.value;
-  if (!validateEmail(emailValidation)) {
-    form.classList.add('active-error');
+
+  const getValue = email.value;
+
+  if (!emailValidation(getValue)) {
+    mainForm.classList.add("active-error");
   } else {
-    form.classList.remove('active-error');
+    mainForm.classList.remove("active-error");
   }
 });
